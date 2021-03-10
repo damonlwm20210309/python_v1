6f08b55e5fc56b317c1523b13b34bf86534ebb21
damonlwm20210309 <L_liu_wei_ming@163.com>
2021-03-10 10:22:45 +0800

    提交

--- demo.py
+++ demo.py
@@ -8,6 +8,7 @@ app = Celery('celery_tasks.tasks', broker='redis://127.0.0.1:6379/0')
 file = "test.txt"
 @app.task
 def add(a, b):
-    c = a+ b + 2
+    c = a + b + 2
+    print('ewreewrrrrrrrrrrr')
     with open(file, 'w') as f:
         f.write(c)
